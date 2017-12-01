b4d681f6337d58ff6ca6652835beb58e4213af0a
Jordyn Middlebrooks <32882292+hjjjcjordyn@users.noreply.github.com>
2017-11-30 23:54:12 -0600

    HW wk 8
    pseudo code in js, html/css for final project

---
+++ 09_starter_extras/final_project_template/js/main.js
@@ -0,0 +1,18 @@
+//if a user clicks on the heart button
+  //make div  move (translate X 100%) to the right and fadeOut
+  //make other div move in (translate X 100% to the right) from the left side of the screen and fadeIn
+
+  //else if user clicks on the X button
+    //make the div move (translate -100%) to the left and fade out
+    //make other div move in (translate X 100% to the right) from the left side of the screen and fadeIn
+
+      //else
+        //the div stays where it is
+
+
+
+//when the user scrolls greater than or equal to 50 px
+  //make the img "../pup.jpeg appear as background img for the header and the footer
+
+//when the user clicks the email button .email
+  //make a modeal text box appear where users can type in their message
